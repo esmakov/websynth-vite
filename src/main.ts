9672864e4f107f3f23b0eaa6ed29cf6d1999cf02
esmakov <edwardsmakov@gmail.com>
2023-06-15 10:35:54 -0400

    Fix: node connections and controls added

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,43 @@
 import "./style.css";
-import Tone from "tone";
+import * as Tone from "tone";
 
 //create a synth and connect it to the main output (your speakers)
-const synth = new Tone.Synth().toDestination();
 const env = new Tone.AmplitudeEnvelope({
   attack: 0.1,
   attackCurve: "exponential",
   decay: 0.2,
   sustain: 1.0,
   release: 0.8,
-});
+}).toDestination();
+const osc = new Tone.Oscillator("C4", "sine").connect(env);
+osc.debug = true;
 
 //play a middle 'C' for the duration of an 8th note
-const button = document.createElement("button");
-button.textContent = "play";
-document.body.appendChild(button);
-button.addEventListener("click", async () => {
+const button = document.querySelector("#play");
+button!.addEventListener("click", async () => {
   await Tone.start();
-  synth.triggerAttackRelease("C4", "8n");
+  if (osc.state !== "started") {
+    osc.start();
+    env.triggerAttack();
+    button!.textContent = "Stop";
+  } else {
+    env.triggerRelease();
+    osc.stop();
+    button!.textContent = "Play";
+  }
+});
+
+const waveformPicker = document.querySelector("#waveform-picker");
+waveformPicker!.addEventListener("change", (e) => {
+  osc.type = e.target.value;
+});
+
+const sliders = document.querySelectorAll("input[type=range]");
+sliders.forEach((slider) => {
+  slider.addEventListener("input", (e) => {
+    const { value, id } = e.target as HTMLInputElement;
+    env[id] = value;
+    console.log(id, env[id]);
+    console.log(env.value);
+  });
 });
